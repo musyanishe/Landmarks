5548530e092c8f44718a3a39d3fa521e867f1fb9
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-11-04 14:42:15 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Евгения Шевцова on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
