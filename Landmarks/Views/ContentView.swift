e25b08d93215197537e74a8fadf5967361fa89fd
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-11-04 17:45:01 +0300

    could't commit at the first time

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Евгения Шевцова on 04.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
